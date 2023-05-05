63a6b51561b601e6aacddde66fb805e4802696fb
Your Name <you@example.com>
2023-05-05 09:25:47 +0200

    basic setup

--- main.py
+++ main.py
@@ -6,10 +6,28 @@ res = (1920,1080) #rozlisenie
 
 screen = pygame.display.set_mode(res)
 
+mainButtonColor = (255,0,0)
+fontButtonColor = (255,255,255)
+WIDTH = 910
+HEIGHT = 540
+font = pygame.font.Font('Font\\rainyhearts.ttf', 50)
 
+
+
+
+
+
+buttonQuit = pygame.draw.rect(screen,mainButtonColor,(WIDTH,HEIGHT, 105,50))
+text = font.render('QUIT', True, fontButtonColor)
+
+screen.blit(text, (WIDTH,HEIGHT))
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    buttonQuit
+
+    pygame.display.update()
