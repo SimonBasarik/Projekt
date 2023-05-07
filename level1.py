cfd3e2d7b5fa1861e1eabc7359fc4acd64b840a8
SimonBasarík <basariksimon@gmail.com>
2023-05-07 11:50:59 +0200

    Vykreslenie hraca (pohyb zatial nefunguje)

--- level1.py
+++ level1.py
@@ -10,7 +10,9 @@ clock = pygame.time.Clock()
 
 running = True
 
-
+playerPositionX = 100
+playerPositionY = 100
+playerIcon = pygame.Rect(playerPositionX, playerPositionY, 50 , 50)
 
 while running:
     screen.fill((0, 0, 0))
@@ -18,5 +20,22 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    pygame.display.update()
+    #Pohyb hraca
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_w]:
+        playerPositionY += 1
+
+
+    if pressed[pygame.K_a]:
+        playerPositionX -= 1
+
+
+    if pressed[pygame.K_d]:
+        playerPositionX += 1
+
+    if pressed[pygame.K_s]:
+        playerPositionY -= 1
+
+    player = pygame.draw.rect(screen, (255, 255, 255), playerIcon)
+    pygame.display.flip()
     clock.tick(60)
