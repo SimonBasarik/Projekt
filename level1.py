1bda501c0bf107d3476a861c9fec6d9eede71e41
SimonBasarík <basariksimon@gmail.com>
2023-05-08 13:43:36 +0200

    Opravenie pohybu, pridanie custom ikony mysi

--- level1.py
+++ level1.py
@@ -1,10 +1,15 @@
 import pygame
 
+PLAYER_SPEED = 5
+
 pygame.init()
 
 res = (1920,1080) #rozlisenie
 
 screen = pygame.display.set_mode(res)
+
+pygame.mouse.set_visible(False)
+cursor = pygame.image.load('slick_arrow-delta.png').convert_alpha()
 
 clock = pygame.time.Clock()
 
@@ -12,10 +17,13 @@ running = True
 
 playerPositionX = 100
 playerPositionY = 100
-playerIcon = pygame.Rect(playerPositionX, playerPositionY, 50 , 50)
+def playerIcon(x,y):
+    return pygame.Rect(int(x),int(y), 50, 50)
 
 while running:
     screen.fill((0, 0, 0))
+    mouse = pygame.mouse.get_pos()
+    mouse_click = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -23,19 +31,18 @@ while running:
     #Pohyb hraca
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_w]:
-        playerPositionY += 1
-
-
-    if pressed[pygame.K_a]:
-        playerPositionX -= 1
-
-
-    if pressed[pygame.K_d]:
-        playerPositionX += 1
+        playerPositionY -= PLAYER_SPEED
 
     if pressed[pygame.K_s]:
-        playerPositionY -= 1
+        playerPositionY += PLAYER_SPEED
 
-    player = pygame.draw.rect(screen, (255, 255, 255), playerIcon)
+    if pressed[pygame.K_a]:
+        playerPositionX -= PLAYER_SPEED
+
+    if pressed[pygame.K_d]:
+        playerPositionX += PLAYER_SPEED
+
+    player = pygame.draw.rect(screen, (255, 255, 255), playerIcon(playerPositionX, playerPositionY))
+    screen.blit(cursor, mouse)
     pygame.display.flip()
     clock.tick(60)
